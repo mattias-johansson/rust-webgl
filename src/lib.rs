8866a72e0fbc2b349de3b8751a49a109c60ad8e0
Mattias Johansson <mattias@Mattiass-MacBook-Pro.local>
2019-09-05 13:23:18 +0200

    clean up code

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,7 @@ use crate::rnd::draw::init_textures;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::{WebGlProgram, WebGlRenderingContext, WebGlShader, WebGlUniformLocation};
-use std::f32::consts::PI;
 use std::rc::Rc;
-use self::rnd::texture_unit::*;
-use self::rnd::lti::*;
-use self::rnd::draw::*;
-use js_sys::WebAssembly;
-use nalgebra_glm as glm;
-use std::cell::RefCell;
 use crate::controls::draw::Draw;
 
 use crate::controls::toggle_button::*;
@@ -108,87 +101,9 @@ impl Application {
         for node in self.node_tree.iter() {
             node.draw(&self.gl, &self.program);
         }
+    }
 }
 
-}
-/*
-#[wasm_bindgen(start)]
-pub fn start() -> Result<(), JsValue> {
-    
-    let mut node_tree : Vec<ToggleButton> = vec![];
-    let tb1 = ToggleButton::new (10.0, 10.0, 0.0);
-    node_tree.push(tb1);
-    let tb2 = ToggleButton::new (30.0, 30.0, 0.0);
-    node_tree.push(tb2);
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    
-    let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>()?;
-
-    let context = canvas
-        .get_context("webgl")?
-        .unwrap()
-        .dyn_into::<WebGlRenderingContext>()?;
-
-    let context = Rc::new(context);
-
-    let vert_shader = compile_shader(
-        &context,
-        WebGlRenderingContext::VERTEX_SHADER,
-      r#"
-        attribute vec4 vertexData;
-        varying vec2 texCoords;
-        
-        uniform mat4 model;
-            uniform mat4 view;
-        uniform mat4 perspective;
-
-        void main() {
-            gl_Position = perspective * view * model * vec4(vertexData.xy, 1.0, 1.0);
-            texCoords = vertexData.zw;
-        }
-    "#,
-    )?;
-
-    let frag_shader = compile_shader(
-        &context,
-        WebGlRenderingContext::FRAGMENT_SHADER,
-    r#"
-        precision mediump float;
-        varying vec2 texCoords;
-        uniform sampler2D texture;
-
-        void main() {
-            gl_FragColor = texture2D( texture, texCoords ); 
-            gl_FragColor.rgb *= gl_FragColor.a;
-        }
-        "#,
-    )?;
-    let program = link_program(&context, &vert_shader, &frag_shader)?;
-    context.use_program(Some(&program));
-
-    let buffer = context.create_buffer().ok_or("failed to create buffer")?;
-    context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
-
-    let f = Rc::new(RefCell::new(None));
-    let g = f.clone();
-
-    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-        context.clear_color(1.0, 1.0, 1.0, 1.0);
-        context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT | WebGlRenderingContext::DEPTH_BUFFER_BIT);
-
- //       render(&context, &program, 107.0, 36.0, 0.0, 0.0, TextureUnit::ToggelBackground);
-       // render(&context, &program, 30.0, 30.0, 3.0, 3.0, TextureUnit::Toggle);
-       // render(&context, &program, 30.0, 30.0, 3.0, 3.0, TextureUnit::ToggleActive);
-
-        // Schedule ourself for another requestAnimationFrame callback.
-        request_animation_frame(f.borrow().as_ref().unwrap());
-    }) as Box<dyn FnMut()>));
-
-    request_animation_frame(g.borrow().as_ref().unwrap());
-    Ok(())
-}
-*/
 pub fn compile_shader(
     context: &WebGlRenderingContext,
     shader_type: u32,
